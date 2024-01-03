37e926d9fe513b0fa3212ee13e39a7f763d4004d
Mario Henrique <mariomapedrao@gmail.com>
2024-01-03 15:37:07 -0300

    upload files

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+
+            <ul class="stats ${pokemon.stat}">
+                <li>HP: ${pokemon.stat}</li>
+                <li>Attack: ${pokemon.attack} </li>
+                <li>Defense: ${pokemon.defense} </li>
+                <li>Special Attack: ${pokemon.specialAttack} </li>
+                <li>Special Defense: ${pokemon.specialDefense} </li>
+                <li>Speed: ${pokemon.speed }</li>
+            </ul>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
